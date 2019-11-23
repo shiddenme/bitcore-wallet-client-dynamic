a3b6a225da88abcde27164b7a3a2e3909fb5977d
Duality-CDOO <s.lievens@duality.solutions>
2019-11-23 11:43:29 -0800

    Fix versioning/deps

--- test/utils.js
+++ test/utils.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 var chai = require('chai');
 var sinon = require('sinon');
 var should = chai.should();
-var Bitcore = require('@duality-solutions/dynamic-lib');
+var Bitcore = require('@duality-solutions/bitcore-lib-dynamic');
 
 var Utils = require('../lib/common/utils');
 
